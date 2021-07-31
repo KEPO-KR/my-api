e66f90c2774bfc62e0dfe73d9713041df52ece0d
KEPO-KR <76137574+KEPO-KR@users.noreply.github.com>
2021-07-31 18:17:32 +0900

    Add files via upload

---
+++ crud/curd-api.js
@@ -0,0 +1,19 @@
+let arr = [];
+function crud(i,title,text,ops,index) {
+  let id = document.getElementById(i);
+  let html = '<h2>'+title+'</h2><br/><span>'+text+'</span>';
+  if (ops=='c') {
+    arr.push(html);
+    id.innerHTML = html;
+  } else if (ops == 'r') {
+    id.innerHTML = arr[index];
+  } else if (ops == 'u') {
+    arr[index]=html;
+    id.innerHTML = html;
+  } else if (ops == 'd') {
+    arr.splice(index);
+    id.innerHTML = '';
+  }
+  console.log(arr);
+}
+//제작자:sangho
